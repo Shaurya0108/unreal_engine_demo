9ea787beaf7def0acce338fbdd2fbe6f2d1ac075
Shaurya Dwivedi <shauryadwivedi0108@gmail.com>
2024-10-16 17:22:44 -0500

    added public functions to make everything work

--- ThirdPerson/Private/Items/MyActor.cpp
+++ ThirdPerson/Private/Items/MyActor.cpp
@@ -1,29 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+
 #include "Items/MyActor.h"
 #include "DrawDebugHelpers.h"
 #include "ThirdPerson/DebugMacros.h"
 
 // Sets default values
-AItem::AItem() {
-	PrimaryActorTick.bCanEverTick = true;
+AMyActor::AMyActor() {
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
 }
 
 // Called when the game starts or when spawned
-void AItem::BeginPlay() {
+void AMyActor::BeginPlay() {
 	Super::BeginPlay();
 }
 
-float AItem::TransformedSin() {
+float AMyActor::TransformedSin() {
 	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
 }
 
-float AItem::TransformedCos() {
+float AMyActor::TransformedCos() {
 	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
 }
 
 // Called every frame
-void AItem::Tick(float DeltaTime) {
+void AMyActor::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
 
 	RunningTime += DeltaTime;
@@ -31,6 +33,8 @@ void AItem::Tick(float DeltaTime) {
 	DRAW_SPHERE_SingleFrame(GetActorLocation());
 	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
 
+	// Template function
 	FVector AvgVector = Avg<FVector>(GetActorLocation(), FVector::ZeroVector);
+
 	DRAW_POINT_SingleFrame(AvgVector);
 }
--- ThirdPerson/Public/Items/MyActor.h
+++ ThirdPerson/Public/Items/MyActor.h
@@ -7,20 +7,36 @@
 #include "MyActor.generated.h"
 
 UCLASS()
-class THIRDPERSON_API AMyActor : public AActor
-{
+class THIRDPERSON_API AMyActor : public AActor {
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	AMyActor();
-
+	virtual void Tick(float DeltaTime) override;
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
+	float Amplitude = 0.25f;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
+	float TimeConstant = 5.f;
+
+	UFUNCTION(BlueprintPure)
+	float TransformedSin();
+
+	UFUNCTION(BlueprintPure)
+	float TransformedCos();
+
+	template<typename T>
+	T Avg(T First, T Second);
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float RunningTime;
 };
+
+template<typename T>
+inline T AMyActor::Avg(T First, T Second) {
+	return (First + Second) / 2;
+}
