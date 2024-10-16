6dcdb2a066a4568ec8aab18314aad71ca554bc95
Shaurya Dwivedi <shauryadwivedi0108@gmail.com>
2024-10-16 17:16:32 -0500

    added transform functions

--- ThirdPerson/Private/Items/MyActor.cpp
+++ ThirdPerson/Private/Items/MyActor.cpp
@@ -1,52 +1,36 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Items/MyActor.h"
 #include "DrawDebugHelpers.h"
 #include "ThirdPerson/DebugMacros.h"
 
 // Sets default values
-AMyActor::AMyActor()
-{
-    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-    PrimaryActorTick.bCanEverTick = true;
+AItem::AItem() {
+	PrimaryActorTick.bCanEverTick = true;
 }
 
 // Called when the game starts or when spawned
-void AMyActor::BeginPlay()
-{
-    Super::BeginPlay();
-    
-    // Log in console
-    UE_LOG(LogTemp, Warning, TEXT("AMyActor::BeginPlay"));
-    
-    // Log on game screen
-    if (GEngine) {
-        GEngine->AddOnScreenDebugMessage(0, 5.0f, FColor::Red, TEXT("On Screen Debug"));
-    }
-    
-    // Define the world
-    UWorld* World = GetWorld();
+void AItem::BeginPlay() {
+	Super::BeginPlay();
+}
 
-	FVector Location = GetActorLocation();
-    FVector Forward = GetActorForwardVector();
+float AItem::TransformedSin() {
+	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
 
-	DRAW_SPHERE(Location);
-	DRAW_VECTOR(Location, Location + Forward * 100.f);
+float AItem::TransformedCos() {
+	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
 }
 
 // Called every frame
-void AMyActor::Tick(float DeltaTime)
-{
-    Super::Tick(DeltaTime);
-    
-    // Log the delta time to the console
-    UE_LOG(LogTemp, Warning, TEXT("Delta Time: %f"), DeltaTime);
+void AItem::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
 
-    // Log the delta time to the screen for 5 seconds
-    if (GEngine) {
-        FString message = FString::Printf(TEXT("AMyActor::Tick: %f"), DeltaTime);
-        GEngine->AddOnScreenDebugMessage(0, 5.0f, FColor::Red, message);
-    }
+	RunningTime += DeltaTime;
+
+	DRAW_SPHERE_SingleFrame(GetActorLocation());
+	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
+
+	FVector AvgVector = Avg<FVector>(GetActorLocation(), FVector::ZeroVector);
+	DRAW_POINT_SingleFrame(AvgVector);
 }
-
