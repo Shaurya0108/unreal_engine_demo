754eb9e592a088454a0231ba21970f5b1b43c5f8
Shaurya Dwivedi <shauryadwivedi0108@gmail.com>
2024-11-05 09:41:40 -0600

    virtual overlap and weapon class

---
+++ ThirdPerson/Public/Items/Weapons/Weapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/MyActor.h"
+#include "Weapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSON_API AWeapon : public AMyActor
+{
+	GENERATED_BODY()
+	
+};
