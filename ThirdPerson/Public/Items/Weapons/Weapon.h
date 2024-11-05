08216e818163edb5b45a80665d68c8d95486b9d6
Shaurya Dwivedi <shauryadwivedi0108@gmail.com>
2024-11-05 09:43:10 -0600

    function def for overlap

--- ThirdPerson/Public/Items/Weapons/Weapon.h
+++ ThirdPerson/Public/Items/Weapons/Weapon.h
@@ -13,5 +13,7 @@ UCLASS()
 class THIRDPERSON_API AWeapon : public AMyActor
 {
 	GENERATED_BODY()
-	
+protected:
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
 };
