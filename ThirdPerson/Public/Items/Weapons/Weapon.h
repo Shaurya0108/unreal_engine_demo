e8add75c8de2eaec1f42f05e24f7a409bbfb2bdd
Shaurya Dwivedi <shauryadwivedi0108@gmail.com>
2024-11-12 14:22:02 -0600

    collision

--- ThirdPerson/Public/Items/Weapons/Weapon.h
+++ ThirdPerson/Public/Items/Weapons/Weapon.h
@@ -6,6 +6,8 @@
 #include "Items/MyActor.h"
 #include "Weapon.generated.h"
 
+class UBoxComponent;
+
 /**
  * 
  */
@@ -14,9 +16,29 @@ class THIRDPERSON_API AWeapon : public AMyActor
 {
 	GENERATED_BODY()
 public:
+	AWeapon();
 	void Equip(USceneComponent* InParent, FName InSocketName);
 	void AttachMeshToSocket(USceneComponent* InParent, const FName& InSocketName);
 protected:
+	virtual void BeginPlay() override;
+
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+	
+	UFUNCTION()
+	void OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Weapon Properties")
+	USoundBase* EquipSound;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	UBoxComponent* WeaponBox;
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* BoxTraceStart;
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* BoxTraceEnd;
 };
